da79b375a047ed4af8814531172a31e0e915134d
nizarmonza <nizarmonzaa@gmail.com>
2022-10-16 16:48:57 +0000

    Some Changes

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -1,21 +1,38 @@
 import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
+import { useNavigate } from "react-router-dom";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+
 
 export const Login = () => {
 	const { store, actions } = useContext(Context);
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
+	const token = sessionStorage.getItem("token");
+	const navigate = useNavigate("");
+	console.log("This is your token", store.token);
+	
+
+	const handleClick = () => {
+		actions.login(email, password)
+		
+		
+	};
 
 	return (
 		<div className="text-center mt-5">
 			<h1>Login</h1>
+			{token && token !="" && token != undefined ? ("You are logged in with this token" + token): (
+
 			<div>
-				<input type="text" placeholder="email" value={email} onChange={() => setEmail(e.target.value)}/>
-				<input type="password" placeholder="password" value={password} onChange={() => setPassword(e.target.value)}/>
-				<button>Login</button>			
+				<input type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)}/>
+				<input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)}/>
+				<button onClick={handleClick}>Login</button>			
 			</div>
+			)}
 		</div>
 	);
 };
+
+
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,6 +1,7 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
+			token: null,
 			message: null,
 			demo: [
 				{
@@ -19,6 +20,50 @@ const getState = ({ getStore, getActions, setStore }) => {
 			// Use getActions to call a function within a fuction
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
+			},
+
+			syncTokenFromSession: () => {
+				const token = sessionStorage.getItem("token");
+				console.log("Application just loaded, syncing the local session storage")
+				if(token &&  token != "" && token != undefined) setStore({ token: token });
+			},
+
+			logout: () => {
+				sessionStore.removeItem("token");
+				console.log("Logging out")
+				setStore({ token: null });
+			},
+
+			login: async (email, password) => {
+				const opts = {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify({
+						"email": email,
+						"password": password
+					})
+				};
+		
+				try{
+				const resp = await fetch('https://3001-nizarmonza-jwtlogin-rde8ss680bv.ws-us69.gitpod.io/api/token', opts)
+				
+				if (resp.status !== 200){
+					
+					alert("There has been some error");
+					return false;
+				} 
+				
+				const data = await resp.json();
+				sessionStorage.setItem('token', data.access_token);
+				setStore({ token: data.access_token})
+				return true;
+				}
+				catch(error) {
+					console.error("There has been an error loggin in")
+				};
+
 			},
 
 			getMessage: async () => {
